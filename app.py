403b9cbb0483881c167868eba933c9818d9facb9
ras592 <rasharrott@icloud.com>
2016-05-05 00:54:02 -0400

    UI updates, contact post logic added

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, session, url_for, flash, redirect
+from flask import Flask, render_template, session, url_for, flash, redirect, request
 
 """
 Clean up remaining code
@@ -8,6 +8,8 @@ Make instagram relflect the right date
 
 
 app = Flask(__name__)
+
+app.secret_key = 'a hard to guess string'
 
 @app.route('/')
 def index():
@@ -19,9 +21,22 @@ def services():
     errors = []
     return render_template('services.html', errors=errors)
 
-@app.route('/contact')
+@app.route('/contact', methods=["GET", "POST"])
 def contact():
     errors = []
+    if request.method == 'POST':
+    	entry = {}
+    	entry['name'] = request.form['name']
+    	entry['email'] = request.form['email']
+    	if 'phone' in request.form:
+    		entry['phone'] = request.form['phone']
+    	else:
+    		entry['phone'] = ""
+    	entry['message'] = request.form['message']
+    	for m in entry:
+    		print(m)
+    	flash('Thank you!')
+    	redirect('/contact')
     return render_template('contact.html', errors=errors)
 
 @app.route('/founders')
--- static/js/features.js
+++ static/js/features.js
@@ -22,8 +22,8 @@ $(document).ready(function() {
         var tools = [
             "<h4>Our Tools</h4>",
             "<ul class=\"icons animated bounceIn\">",
-                "<li><a href=\"http://www.adobe.com/products/photoshop/selector.html\"><img src=\"/static/images/photoshop.png\" width=\"50%\"></a></li>",
-                "<li><a href=\"https://www.adobe.com/products/photoshop-lightroom.html\"><img src=\"/static/images/lightroom.png\" width=\"50%\"></a></li>",
+                "<li><a href=\"http://www.adobe.com/products/photoshop/selector.html\"><img class=\"tool_img\" src=\"/static/images/photoshop.png\" width=\"50%\"></a></li>",
+                "<li><a href=\"https://www.adobe.com/products/photoshop-lightroom.html\"><img class=\"tool_img\" src=\"/static/images/lightroom.png\" width=\"50%\"></a></li>",
             "</ul>",
         ].join("");
         var og = [
